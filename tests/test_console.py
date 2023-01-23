de5bb39e84bb69ab542c92647ada8410fba25003
iakev <kirimikmwongera@gmail.com>
2023-01-23 11:58:38 +0000

    added test module for the console module

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module for testing console"""
+import unittest
+import console
+
+
+class test_console(unittest.TestCase):
+    """ Class to test the console method"""
+
+    def test_console(self):
+        """ test that console module exists"""
+        pass
