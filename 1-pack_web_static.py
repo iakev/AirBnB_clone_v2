ad50044331e1800c7d018f5c9cd9fa778c4188fd
iakev <kirimikmwongera@gmail.com>
2023-01-30 16:08:21 +0000

    added a fabric script taht generates a .tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A fabric script to generate a .tgz archive from the contents
+of the web_static folder
+"""
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """"
+    A function to create a folder and an archive to store
+    web_static folder contents
+    """
+
+    local("mkdir -p versions")
+    date_str = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "web_Static_{}.tgz".format(date_str)
+    result = local("tar -cvzf versions/{} web_static".format(file_name))
+    if result.failed:
+        return None
+    else:
+        return "versions/{}".format(file_name)
