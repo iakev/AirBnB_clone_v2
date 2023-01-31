48387402f287f5d520637b121ae15d958aa16062
iakev <kirimikmwongera@gmail.com>
2023-01-31 09:24:53 +0000

    added a Fabric script that distributes an archive to we_servers using a functionn'do_deploy'

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Script to distribute archives to web servers usinf 'do_deploy'
+function
+"""
+from fabric.api import run, put, env
+import os
+env.user = "ubuntu"
+env.hosts = ['18.204.5.6', '100.26.241.83']
+env.key_filename = "/home/vagrant/.ssh/school"
+
+
+def do_deploy(archive_path):
+    """
+    uploads archives to web servers, ucompresses the archives
+    deletes the archives
+    """
+
+    if os.path.exists(archive_path):
+        result = put(archive_path, "/tmp/")
+        if result.succeeded:
+            name_with_ext = archive_path.replace("versions/", "")
+            name_wto_ext = name_with_ext.replace(".tgz", "")
+            result = run("mkdir -p /data/web_static/releases/{}"
+                         .format(name_wto_ext))
+            if result.succeeded:
+                result = run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+                             .format(name_with_ext, name_wto_ext))
+                if result.succeeded:
+                    result = run("mv -f /data/web_static/releases/{}/"
+                                 "web_static/* /data/web_static/releases/{}"
+                                 .format(name_wto_ext, name_wto_ext))
+                    if result.succeeded:
+                        result = run("rm /tmp/{}".format(name_with_ext))
+                        if result.succeeded:
+                            result = run("rm -rf /data/web_static/current")
+                            if result.succeeded:
+                                result = run("ln -s /data/web_static/releases/"
+                                             "{} /data/web_static/current"
+                                             .format(name_wto_ext))
+                                if result.succeeded:
+                                    print("New version deployed!")
+                                else:
+                                    return False
+                            else:
+                                return False
+                        else:
+                            return False
+                    else:
+                        return False
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
