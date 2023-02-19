4186ca79956ce8aa42345929db1cfb834af748e6
iakev <kirimikmwongera@gmil.com>
2023-02-19 16:06:01 +0000

    added a test for create method of cmd

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,6 +2,23 @@
 """ Module for testing console"""
 import unittest
 import console
+import os
+import io
+from contextlib import redirect_stdout
+from models import storage
+from models.base_model import BaseModel
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+HBNBComm = console.HBNBCommand()
+classes = {"BaseModel": BaseModel, "State": State, "User": User,
+           "Place": Place, "City": City, "Amenity": Amenity,
+           "Review": Review}
 
 
 class test_console(unittest.TestCase):
@@ -9,4 +26,15 @@ class test_console(unittest.TestCase):
 
     def test_console(self):
         """ test that console module exists"""
-        pass
+        self.assertTrue(os.path.isfile('console.py'))
+
+    def test_create_wto_params(self):
+        """test that console creates without params"""
+        with redirect_stdout(io.StringIO()) as f:
+            HBNBComm.do_create("State")
+        id = f.getvalue()
+        id = id.replace('\n', "")
+        c_name = "State"
+        key = c_name + "." + id
+        created_state = storage.all()[key]
+        self.assertEqual(id, created_state.id)
