a973c5af4b37e8f57c5b3f488cbad16cd276ed4f
iakev <kirimikmwongera@gmil.com>
2023-03-07 06:34:48 +0000

    added a dual route with optional default parameters

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+start flask application and display something
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_flask():
+    """ return first content """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """another route to hbnb addded"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_isfun(text):
+    """a route with a parameter"""
+    try:
+        text = text.replace('_', ' ')
+    except Exception as e:
+        pass
+    return "C {}".format(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """a route with variable parameters but defult value"""
+    try:
+        text = text.replace('_', ' ')
+    except Exception as e:
+        pass
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
