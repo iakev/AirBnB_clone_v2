2209d110f8a9d406eacc91d0bb7dd1a4a81ab0dc
iakev <kirimikmwongera@gmil.com>
2023-03-14 12:28:46 +0000

    added a delete method to the file engine and also modified all to perform an optional filtering

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        all_objs = FileStorage.__objects
+        if cls:
+            cls_objs = {}
+            if cls in classes:
+                for k, v in all_objs:
+                    if str(cls) == v.to_dict()['__class__']:
+                        cls_objs[k] = v
+                return cls_objs
+        return all_objs
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +66,15 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """
+        add a delete feature which removes obj from __objects
+        """
+        if obj:
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+            objs = self.all()
+            key_obj = objs.get(key)
+            if key_obj:
+                del objs[key]
+        
